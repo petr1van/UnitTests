ae924df7e3854552f10adea691e8a7e52830db84
Peter Ivanov <102377257+petr1van@users.noreply.github.com>
2022-12-26 19:46:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.listen(PORT, () => console.log('server started on post ${PORT}'))
+
+app.get('/',(req, res)) => {
+    res.send('Succes')
+}
